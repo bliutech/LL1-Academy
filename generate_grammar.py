97fe74d40d29c08dac2821af92063214d7e1a245
Benson Liu <bensonhliu@gmail.com>
2024-05-18 14:20:21 -0700

    feat: updated `generate_grammar.py` for exploring

--- generate_grammar.py
+++ generate_grammar.py
@@ -3,20 +3,20 @@ Checking out the LL1_Academy grammar generation libraries
 """
 
 from LL1_Academy.tools.SingleGrammarGenerator import SingleGrammarGenerator
-from LL1_Academy.tools.MassGrammarGenerator import MassGrammarGenerator
+from LL1_Academy.tools.GrammarChecker import GrammarChecker
 import string, random
+
 
 def generate_grammar(nonTerminals, terminals):
     g = SingleGrammarGenerator()
     return g.generate(nonTerminals, terminals)
 
-def mass_generate_grammar(num, nonTerminals, terminals):
-    mg = MassGrammarGenerator(num)
-    mg.run(num, nonTerminals, terminals)
-
 
 potentialNonTerminals = list(string.ascii_uppercase)
-potentialTerminals = list(string.ascii_lowercase) + ['(', ')', '+', '*']
+potentialTerminals = list(string.ascii_lowercase)
+
+potentialNonTerminals = list("AB")
+potentialTerminals = list("wxyz")
 
 num_grammars = 100
 for i in range(num_grammars):
@@ -24,4 +24,14 @@ for i in range(num_grammars):
     print(nonTerminals)
     terminals = random.sample(potentialTerminals, random.randint(2, 4))
     print(terminals)
-    mass_generate_grammar(2, nonTerminals, terminals)
+    g = generate_grammar(nonTerminals, terminals)
+    gc = GrammarChecker()
+    firstSets, followSets, parsingTable, status, reachable, terminals = gc.solve(g, nonTerminals[0], False)
+    print("First sets:", firstSets)
+    print("Follow sets:", followSets)
+    print("Parsing table:", parsingTable)
+    print("Is LL(1):", "yes" if status == 0 else "no" if status == 1 else "left recursion")
+    print("Reachable:", reachable)
+    print("Terminals:", terminals)
+    print("Grammar:", g)
+    breakpoint()
